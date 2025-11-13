54fad9eb29b7154153b8201dcf997844d0e00a0d
Solux S. <matheussouza.silva@a.ucb.br>
2025-11-12 23:20:30 -0300

    reinicio total

--- src/includes/screens.h
+++
@@ -1,8 +0,0 @@
-#ifndef SCREENS_H
-#define SCREENS_H
-
-void welcomeScreen();
-void login();
-void cadastro();
-
-#endif // SCREENS_H
--- src/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "includes/screens.h"
-
-int main() {
-    welcomeScreen();
-    return 0;
-}
